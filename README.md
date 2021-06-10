41c94021cfa6cc31148e808e0c5ba72bf0da7375
Atharvious <atharva.3015@gmail.com>
2021-06-10 17:56:12 +0530

    added Extractive&Abstract summarization module

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
-# telegram-ocr-bot
+## Nice and simple ocr bot for telegram. Built just because I didn't wanna install a seperate ocr app on my phone (and pc too for that matter).
 
-Nice and simple ocr bot for telegram. Built just because I didn't wanna install another app on my phone.
+The repository contains all the source code needed to reproduce the bot. The Dockerfile is pre-configured as well (but see the note below).
 
-The repository contains the source code to reproduce the bot. Select the python interpreter from /env to run the scripts, as all dependencies are provided in the virtual environment. However, a bot token is needed to make it work - Get a token from BotFather on telegram, and use it in line 12 in bot.py. 
+*A bot token is needed to make it work - Get a token from '**@BotFather**' on telegram, and use it in line 12 in **bot.py**.*
+
+**To run without docker:**
+* Select the python interpreter from env/Scripts.
+* Make sure you have tesseract installed and added to PATH.
+  * For **Windows** users, even if you don't have the tesseract engine up and running, you can run it by uncommenting line 2 in **ocr.py**, as I have provided a tesseract build in the repo itself.
+  * For **linux** users, just have tesseract in PATH and don't change anything. You can also change line 2 in **ocr.py** to point to your tesseract installation, if not in PATH.
+
+
+
+### To see the bot in action, search for '**@tescannerbot**' in Telegram and add it.
+
+Possible issues: 
+* If running on Linux, you might need to install the packeges in requirements.txt in a local environment, as opencv-python tends to face dependency problems.
---
+++ nlp.py
@@ -0,0 +1,96 @@
+#--------------------------Imports--------------------------
+
+#For Extractive Summarization
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.cluster.util import cosine_distance
+import numpy as np
+import networkx as nx
+
+#For Abstract Summarization
+import torch
+import json
+from transformers import T5Tokenizer, T5ForConditionalGeneration, T5Config
+
+#--------------------------Extractive Classificaiton--------------------------
+
+class ExtractiveSummarizer:
+    def __init__(self):
+        self.stop_words = set(stopwords.words("english"))
+
+    def summarize(self, text):
+        words = word_tokenize(text)
+        sentences = sent_tokenize(text)
+        #-----------------Word scoring-----------------
+        frequency_table = {}
+        for word in words:
+            word = word.lower()
+            if word in self.stop_words:
+                continue
+            if word in frequency_table:
+                frequency_table[word] += 1
+            else:
+                frequency_table[word] = 1
+        #-----------------Sentence scoring-------------
+        sentence_values = {}
+        for sentence in sentences:
+            for word, freq in frequency_table.items():
+                if word in sentence.lower():
+                    if sentence in sentence_values:
+                        sentence_values[sentence] += freq
+                    else:
+                        sentence_values[sentence] = freq
+        #----------------------------------------------
+        
+        value_sum = 0
+        for sentence in sentence_values:
+            value_sum += sentence_values[sentence]
+        
+        avg = int(value_sum / len(sentence_values))
+
+        summary = ""
+
+        for sentence in sentences:
+            if (sentence in sentence_values) and (sentence_values[sentence] > (1.3 * avg)):
+                summary += " " + sentence
+        return summary
+
+#--------------------------Abstract Classification--------------------------
+class AbstractSummarizer:
+    def __init__(self):
+        self.model = T5ForConditionalGeneration.from_pretrained('t5-small')
+        self.tokenizer = T5Tokenizer.from_pretrained('t5-small')
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    
+    def summarize(self, text):
+        
+        preprocess_text = text.strip().replace("\n","")
+        t5_prepared_text = "summarize: " + preprocess_text
+        tokenized_text = self.tokenizer.encode(t5_prepared_text, return_tensors="pt").to(self.device)
+
+        summary_ids = self.model.generate(
+            tokenized_text,
+            num_beams=4,
+            no_repeat_ngram_size=2,
+            min_length = 50,
+            max_length = 150,
+            early_stopping = True,
+            )
+        summary = self.tokenizer.decode(summary_ids[0],
+                    skip_special_tokens = True,
+        )
+        return summary
+
+def main():
+    with open("sample_text.txt", 'r') as f:
+        sample_text = f.read()
+    print(f"Sample Text:\n{sample_text}\n Number of Characters:\t {len(sample_text)}\n\n")
+    Asummarizer = AbstractSummarizer()
+    Esummarizer = ExtractiveSummarizer()
+    extractive_summary = Esummarizer.summarize(sample_text)
+    abstract_summary = Asummarizer.summarize(sample_text)
+    print(f"Extractive Summary:\n{extractive_summary}\n Number of Characters:\t {len(extractive_summary)}\n\n")
+    print(f"Abstract Summary:\n{abstract_summary}\n Number of Characters:\t {len(abstract_summary)}")
+
+if __name__ == '__main__':
+    main()
