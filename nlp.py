bd9a7694d3bf0c3046177a6bf03a29ddd00d2c5c
Atharvious <atharva.3015@gmail.com>
2021-06-11 01:03:57 +0530

    minor changes

--- nlp.py
+++ nlp.py
@@ -13,7 +13,6 @@ import json
 from transformers import T5Tokenizer, T5ForConditionalGeneration, T5Config
 
 #--------------------------Extractive Classificaiton--------------------------
-
 class ExtractiveSummarizer:
     def __init__(self):
         self.stop_words = set(stopwords.words("english"))
@@ -21,7 +20,7 @@ class ExtractiveSummarizer:
     def summarize(self, text):
         words = word_tokenize(text)
         sentences = sent_tokenize(text)
-        #-----------------Word scoring-----------------
+        #-----------------Word scoring--------------------
         frequency_table = {}
         for word in words:
             word = word.lower()
@@ -31,7 +30,7 @@ class ExtractiveSummarizer:
                 frequency_table[word] += 1
             else:
                 frequency_table[word] = 1
-        #-----------------Sentence scoring-------------
+        #-----------------Sentence scoring----------------
         sentence_values = {}
         for sentence in sentences:
             for word, freq in frequency_table.items():
@@ -40,8 +39,8 @@ class ExtractiveSummarizer:
                         sentence_values[sentence] += freq
                     else:
                         sentence_values[sentence] = freq
-        #----------------------------------------------
-        
+        #----------------Calculate Average------------------
+
         value_sum = 0
         for sentence in sentence_values:
             value_sum += sentence_values[sentence]
@@ -55,7 +54,7 @@ class ExtractiveSummarizer:
                 summary += " " + sentence
         return summary
 
-#--------------------------Abstract Classification--------------------------
+#-----------------------------------------------Abstract Classification--------------------------------------------
 class AbstractSummarizer:
     def __init__(self):
         self.model = T5ForConditionalGeneration.from_pretrained('t5-small')
@@ -63,7 +62,6 @@ class AbstractSummarizer:
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
     
     def summarize(self, text):
-        
         preprocess_text = text.strip().replace("\n","")
         t5_prepared_text = "summarize: " + preprocess_text
         tokenized_text = self.tokenizer.encode(t5_prepared_text, return_tensors="pt").to(self.device)
@@ -80,17 +78,24 @@ class AbstractSummarizer:
                     skip_special_tokens = True,
         )
         return summary
+#--------------------------------------------------Test Function--------------------------------------------------
 
-def main():
+def test():
     with open("sample_text.txt", 'r') as f:
         sample_text = f.read()
     print(f"Sample Text:\n{sample_text}\n Number of Characters:\t {len(sample_text)}\n\n")
+
+
     Asummarizer = AbstractSummarizer()
     Esummarizer = ExtractiveSummarizer()
+
+
     extractive_summary = Esummarizer.summarize(sample_text)
-    abstract_summary = Asummarizer.summarize(sample_text)
     print(f"Extractive Summary:\n{extractive_summary}\n Number of Characters:\t {len(extractive_summary)}\n\n")
+
+    abstract_summary = Asummarizer.summarize(sample_text)
     print(f"Abstract Summary:\n{abstract_summary}\n Number of Characters:\t {len(abstract_summary)}")
 
+#-----------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
-    main()
+    test()
--- ocr.py
+++ ocr.py
@@ -4,7 +4,6 @@ import cv2
 from PIL import Image
 import numpy as np
 class TextScanner():
-
     def __init__(self):
         pass
 
